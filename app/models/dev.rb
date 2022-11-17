a0c0dc6f0de71ff8ba11880fe54fe176f03fd48d
Christian <cedenoc181@gmail.com>
2022-11-17 09:08:30 -0500

    commit tree a7a15df5c7a7c14cdd59daacaba81e65a49ddb3d

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,4 @@
 class Dev < ActiveRecord::Base
-
+has_many :freebies 
+has_many :companys, through: :freebies
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
 puts "Creating companies..."
-Company.create(name: "Google", founding_year: 1998)
-Company.create(name: "Facebook", founding_year: 2004)
-Company.create(name: "Dunder Mifflin", founding_year: 2002)
-Company.create(name: "Enron", founding_year: 1995)
+google = Company.create(name: "Google", founding_year: 1998)
+facebook = Company.create(name: "Facebook", founding_year: 2004)
+dunder_mifflin = Company.create(name: "Dunder Mifflin", founding_year: 2002)
+enron = Company.create(name: "Enron", founding_year: 1995)
 
 puts "Creating devs..."
-Dev.create(name: "Rick")
-Dev.create(name: "Morty")
-Dev.create(name: "Mr. Meseeks")
-Dev.create(name: "Gazorpazop")
+rick = Dev.create(name: "Rick")
+morty = Dev.create(name: "Morty")
+mr_meseeks = Dev.create(name: "Mr. Meseeks")
+gazorpazop = Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
@@ -17,5 +17,13 @@ puts "Creating freebies..."
 # * and a freebie belongs to a company.                         *
 # ***************************************************************
 # Create freebies Here
+pens = Freebie.create(item_name:"pens" , value: 100 , company_id: 3, dev_id: 1) 
+mugs = Freebie.create(item_name:"mugs", value: 25 , company_id: 2, dev_id:4 )
+books = Freebie.create(item_name:"books" , value: 30 , company_id: 1, dev_id: 2)
+braclets = Freebie.create(item_name:"braclets" , value: 200 , company_id: 4, dev_id: 4)
+t_shirts = Freebie.create(item_name:"t_shirts" , value:50, company_id: 3, dev_id: 3)
+hoodies = Freebie.create(item_name:"hoodies" , value: 20 , company_id: 2, dev_id: 1)
+hats = Freebie.create(item_name:"hats" , value:40 , company_id: 1, dev_id: 2)
+
 
 puts "Seeding done!"
